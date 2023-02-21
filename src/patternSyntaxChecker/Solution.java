91d2aef9caa2f2fef6e2533194735b194bc5afe7
valentinj <valentin.jevsejev@gmail.com>
2023-02-21 12:59:24 +0200

    PatternSyntaxChecker solved

--- src/patternSyntaxChecker/Solution.java
+++ src/patternSyntaxChecker/Solution.java
@@ -1,4 +1,27 @@
 package patternSyntaxChecker;
 
+import java.util.Scanner;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
+
 public class Solution {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int testCases = Integer.parseInt(in.nextLine());
+        while(testCases>0){
+            try{
+                String pattern = in.nextLine();
+                Pattern.compile(pattern);
+                System.out.println("Valid");
+            }catch(PatternSyntaxException e){
+                System.out.println("Invalid");
+            }finally{
+                testCases--;
+            }
+        }
+        in.close();
+    }
 }
+
+
+
